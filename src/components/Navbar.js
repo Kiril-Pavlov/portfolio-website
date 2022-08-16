46d90f9f44349dbc926925b78b8e2a1f66e4dd45
Kiril-Pavlov <Kire_P@yahoo.com>
2022-08-16 22:36:33 +0200

    Navbar responsive done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Navbar.css"
 import { FiMenu, FiX } from "react-icons/fi"
 
 const Navbar = ({ navbarLinks }) => {
 
-  
+  const [menuClicked, setmenuClicked] = useState(false)
+
+  const toggleMenuClicked = ()=>{
+    setmenuClicked(!menuClicked);
+  }
 
   return (
     <nav className='navbar'>
       <span className='navbar-logo'>
-          &lt;Kiril-Pavlov - Software Developer/&gt;
+          &lt;Kiril Pavlov - Software Developer/&gt;
       </span>
-      <ul className='navbar-list'>
+      {!menuClicked ? (
+        <FiX size={25} className="hamburger-button" onClick={toggleMenuClicked} />
+        ):(
+        <FiMenu size={25} className="hamburger-button" onClick={toggleMenuClicked} />
+      )}
+
+      <ul className={menuClicked ? "navbar-list" : "navbar-list navbar-list-active"}>
         {navbarLinks.map((item) => {
           return (
             <li className='navbar-list-item' key={item.title}>
