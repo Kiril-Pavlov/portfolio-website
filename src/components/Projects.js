96f40e8fd0ad9d84ef4097bc87a81ee5f2765cc1
Kiril-Pavlov <Kire_P@yahoo.com>
2022-08-22 20:02:18 +0200

    Projects component - added projects, changed css

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,44 +1,59 @@
 import React from 'react'
 import "./Projects.css"
 import todoImg from "../assets/todo-app.png"
+import pizzaImg from "../assets/pizza-app.png"
 
 const Projects = () => {
     const projectsList = [
         {
-            name:"ToDo App",
-            img:`${todoImg}`,
+            name: "ToDo App",
+            img: `${todoImg}`,
             description: "A ToDo App using React.js",
-            skills:["html","css", "react.js"]
+            skills: ["html", "css", "react.js"]
+        },
+        {
+            name: "Pizza App",
+            img: `${pizzaImg}`,
+            description: "Pizza App in React with MongoDB backend",
+            skills: ["html","css","react.jd", "mongodb"]
+        },
+        {
+            name: "Pizza App",
+            img: `${pizzaImg}`,
+            description: "Pizza App in React with MongoDB backend",
+            skills: ["html","css","react.jd", "mongodb"]
         }
     ]
-  return (
-    <div className='projects-container'>
-        {projectsList.map((item)=>{
-            return(
-                <div className='card-container' key={item.name}>
-                    
-                    <div className='skills-used' 
-                        style={{ backgroundImage: `url(${item.img})`,
-                                backgroundRepeat:"no-repeat",
-                                backgroundSize:"cover",
-                                backgroundPosition:"center", 
-                                height:220,
-                                width:450 }
-                        }>
-                        {item.skills.join(" - ")}
+    return (
+        <div className='projects-container'>
+            {projectsList.map((item) => {
+                return (
+                    <div className='card-container' key={item.name}>
+
+                        <div className='skills-used'
+                            style={{
+                                backgroundImage: `url(${item.img})`,
+                                backgroundRepeat: "no-repeat",
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                height: 220,
+                                width: 450
+                            }
+                            }>
+                            {item.skills.join(" - ")}
+                        </div>
+                        <div className='short-description'>
+                            <h3>{item.name}</h3>
+                            <p>{item.description}</p>
+                            <button className='more-info'>
+                                More Info ...
+                            </button>
+                        </div>
                     </div>
-                    <div className='short-description'>
-                        <h3>{item.name}</h3>
-                        <p>{item.description}</p>
-                        <button className='more-info'>
-                            More Info ...
-                        </button>
-                    </div>
-                </div>
-            )
-        })}
-    </div>
-  )
+                )
+            })}
+        </div>
+    )
 }
 
 export default Projects
