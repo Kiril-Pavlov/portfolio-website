6aa66f09fcd5460ec62d419d11b75b6bc3da8768
Kiril-Pavlov <Kire_P@yahoo.com>
2022-08-26 20:32:41 +0200

    navbar logic fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,22 +6,22 @@ const Navbar = ({ navbarLinks }) => {
 
   const [menuClicked, setmenuClicked] = useState(false)
 
-  const toggleMenuClicked = ()=>{
+  const toggleMenuClicked = () => {
     setmenuClicked(!menuClicked);
   }
 
   return (
     <nav className='navbar'>
       <span className='navbar-logo'>
-          &lt;Kiril Pavlov - Software Developer/&gt;
+        &lt;Kiril Pavlov - Software Developer/&gt;
       </span>
-      {!menuClicked ? (
+      {menuClicked ? (
         <FiX size={25} className="hamburger-button" onClick={toggleMenuClicked} />
-        ):(
+        ) : (
         <FiMenu size={25} className="hamburger-button" onClick={toggleMenuClicked} />
       )}
 
-      <ul className={menuClicked ? "navbar-list" : "navbar-list navbar-list-active"}>
+      <ul className={!menuClicked ? "navbar-list" : "navbar-list navbar-list-active"}>
         {navbarLinks.map((item) => {
           return (
             <li className='navbar-list-item' key={item.title}>
