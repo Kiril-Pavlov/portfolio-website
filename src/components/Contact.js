b4125ec5b89aba4d8ee9345555167f7eac451809
Kiril-Pavlov <Kire_P@yahoo.com>
2022-08-27 17:06:11 +0200

    fixed contact form to empty fields after submit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
 
 import "./Contact.css"
@@ -6,15 +6,22 @@ import "./Contact.css"
 const Contact = () => {
     const form = useRef();
 
+    const [userName, setUserName] = useState("");
+    const [userEmail, setUserEmail] = useState("");
+    const [message, setMessage] = useState("");
+
     const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_9avkhj7', 'template_olvqapq', form.current, 'HuHStPwtpkKj-c0y9')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
+        e.preventDefault();
+
+        emailjs.sendForm('service_9avkhj7', 'template_olvqapq', form.current, 'HuHStPwtpkKj-c0y9')
+            .then((result) => {
+                console.log(result.text);
+                setUserName("");
+                setUserEmail("");
+                setMessage("");
+            }, (error) => {
+                console.log(error.text);
+            });
     };
 
     return (
@@ -22,13 +29,25 @@ const Contact = () => {
             <h3>Contact</h3>
             <form className='contact-form' ref={form} onSubmit={sendEmail}>
                 <label>Name</label>
-                <input type="text" name="user_name" />
+                <input type="text"
+                    value={userName}
+                    onChange={event => setUserName(event.target.value)}
+                    name="user_name"
+                />
 
                 <label>Email</label>
-                <input type="email" name="user_email" />
+                <input type="email" 
+                value={userEmail} 
+                onChange={event => setUserEmail(event.target.value)}
+                name="user_email" 
+                />
 
                 <label>Message</label>
-                <textarea name="message" rows={4}/>
+                <textarea name="message" 
+                value={message} 
+                onChange={event => setMessage(event.target.value)}
+                rows={4} 
+                />
 
                 <input type="submit" value="Send" />
             </form>
